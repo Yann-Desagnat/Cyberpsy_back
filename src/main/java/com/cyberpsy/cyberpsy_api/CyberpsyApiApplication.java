83cd0b589499278af91452340e21a3a20f572d7f
Yann Desagnat <yann.desagnat@esme.fr>
2024-12-02 14:47:52 +0100

    first commit

---
+++ src/main/java/com/cyberpsy/cyberpsy_api/CyberpsyApiApplication.java
@@ -0,0 +1,13 @@
+package com.cyberpsy.cyberpsy_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberpsyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyberpsyApiApplication.class, args);
+	}
+
+}
